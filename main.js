7557873ab95ba4b3b1e73f9e320a30508057b892
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-06-07 13:05:34 -0400

    Add functionality to list. Add text from input field to li created via JavaScript

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log('connected');
+
+
+var addButton = document.getElementById('addItem');
+
+//this function is triggered by an event listener (in this case a click)
+addButton.addEventListener('click', function addToDos() {
+
+//take value of what is typed in text input using .value method
+  var newItem = document.getElementById('enterItem').value;
+
+//creating a variable of what HTML element we want to target
+  var list = document.getElementById('listItems');
+
+//creates place for text to go in newly created li elements
+  var textNode = document.createTextNode(newItem);
+
+//creates list items where there were no list items before
+  var added = document.createElement('li');
+
+//turning value from newItem into text node --unfortunately, we will get a typeError if we don't create a new node
+  var t = document.createTextNode(newItem);
+
+//appending text node content to li
+  added.appendChild(t);
+
+  //appending these items to our HTML element "listItems" (list being previously defined variable);
+  list.appendChild(added);
+
+//clear text box upon clicking 'add item'
+  document.getElementById('enterItem').innerHTML = ' ';
+  });
