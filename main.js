1afa9c2dcc7084f3117d1ce56ce17698b96261d4
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-06-07 21:35:06 -0400

    Append delete button with li's

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
 console.log('connected');
 
+/*---------------------------------------ADD ITEM TO LIST-----------------------------------------*/
 
 var addButton = document.getElementById('addItem');
 
-//this function is triggered by an event listener (in this case a click)
+//event listener to trigger function (in this case a click);
 addButton.addEventListener('click', function addToDos() {
 
-//take value of what is typed in text input using .value method
+//take value of what is typed in text input using .value method;
   var newItem = document.getElementById('enterItem').value;
 
-//creating a variable of what HTML element we want to target
+//create a variable of what HTML element we want to target (like a container);
   var list = document.getElementById('listItems');
 
-//creates place for text to go in newly created li elements
-  var textNode = document.createTextNode(newItem);
-
-//creates list items where there were no list items before
+//create list items where there were no list items before;
   var added = document.createElement('li');
 
-//turning value from newItem into text node --unfortunately, we will get a typeError if we don't create a new node
+//create place for text to go in newly created li elements;
+  var textNode = document.createTextNode(newItem);
+
+//turn value from newItem into text node --unfortunately, we will get a typeError if we don't create a new node;
   var t = document.createTextNode(newItem);
 
-//appending text node content to li
+  var newBtn = document.createElement('button');
+
+
+  var deleteText = newBtn.innerHTML = 'delete';
+
+  console.log(added, newBtn);
+
+//append text node content to li;
   added.appendChild(t);
 
-  //appending these items to our HTML element "listItems" (list being previously defined variable);
+  //append these items to our HTML element "listItems" (list being previously defined variable);
   list.appendChild(added);
 
-//clear text box upon clicking 'add item'
+  list.appendChild(newBtn);
+
+//Unnecessary:
+//clear text box upon clicking 'add item';
   document.getElementById('enterItem').value = ' ';
   });
+
+
+/*---------------------------------------DELETE ITEM FROM LIST-----------------------------------------*/
+
+  //i want a delete button for each list item that triggers a modal pop up for asking user if they want to delete;
